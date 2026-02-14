d4e1de3183d0ffb41ad15c005d183bd45c294115
AnyaH0831 <anyahuang0831@gmail.com>
2026-02-14 15:35:28 -0500

    /

--- api/send_email.js
+++ api/send_email.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://personal-website-renovation.vercel.app/'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://personal-website-renovation.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
